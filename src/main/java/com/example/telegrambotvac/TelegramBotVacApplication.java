4331b6bf4685281af33209b00f0b0433cf6f3380
storozhenko <storozhenko-88@ukr.net>
2023-05-27 12:53:28 +0300

    Initial commit

---
+++ src/main/java/com/example/telegrambotvac/TelegramBotVacApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotvac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotVacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotVacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegrambotvac/TelegramBotVacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotvac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotVacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
